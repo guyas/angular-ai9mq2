9aadb74d3d197ee8cd2b2503e1944bc19671d812
guyas <guy.ashiri@gmail.com>
2020-04-26 11:16:52 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ai9mq2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ai9mq2)
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,6 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice', song_path: 'hero11.mp3' },
+  { id: 12, name: 'Narco', song_path: 'hero12.ogg'  }
+];
